2ac6323fc78701d22975bf4fe9433418cb9ed24a
Flora Barbosa Santiago <oliveira.flora@hotmail.com>
2022-03-22 18:13:54 -0300

    Parte Superior

--- src/App.vue
+++ src/App.vue
@@ -1,118 +1,98 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <!-- <v-navigation-drawer
-        v-model="drawer"
-        app
-    >
-      <v-sheet
-        color="grey lighten-4"
-        class="pa-4"
-      >
-        <v-avatar>
-          <img
-            src="https://tc.com.br/wp-content/uploads/2021/07/steve-jobs.jpg"
-            alt="John"
-          >
-        </v-avatar>
-        <div>josemaria@gmail.com</div>
-      </v-sheet>
-      <v-divider></v-divider>
-      <v-list>
-        <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar> -->
-    
-            
-      <v-card 
-        color="grey lighten-4"
-        flat
-        height="1000px" class="photo1"
-        tile
-      >
-        <v-img
-        >
+    <div id="app">
+        <v-app id="inspire">
+            <v-card class="overflow-hidden" height="2800">
+                <v-app-bar color="transparent">
+                    <v-app-bar-nav-icon
+                        @click="drawer = true"
+                        color="white"
+                    ></v-app-bar-nav-icon>
 
-        </v-img>
-        <v-toolbar dense class="photo1">
-          <v-app-bar-nav-icon
-          color="white">
-          </v-app-bar-nav-icon>
+                    <v-btn text color="white">Home</v-btn>
+                    <v-btn text color="white">Produtos</v-btn>
+                    <v-btn text color="white">Preços</v-btn>
+                    <v-btn text color="white">Sobre</v-btn>
+                    <v-btn text color="white">Contato</v-btn>
 
-          <v-spacer></v-spacer>
- 
-    
-          <v-btn icon>
-            <v-icon
-            color="white">
-            mdi-twitter
-            </v-icon>
-          </v-btn>
+                    <!-- <v-spacer></v-spacer>
+                   
+                    <v-img class = "logo" 
+                    src="src/img/logo.png"
+                    ></v-img> -->
 
-          <v-btn icon>
-            <v-icon
-            color="white">
-            mdi-facebook
-            </v-icon>
-          </v-btn>
-    
-          <v-btn icon>
-            <v-icon
-            color="white">
-            mdi-linkedin</v-icon>
-          </v-btn>
-    
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
+                    <v-spacer></v-spacer>
 
-          <v-img class = "logo"
-            lazy-src="https://picsum.photos/id/11/10/6"
-            max-height="150"
-            max-width="250"
-            src="src/img/logo.png"
-          ></v-img>
+                    <v-btn icon>
+                        <v-icon color="white"> mdi-twitter </v-icon>
+                    </v-btn>
 
-        </v-toolbar>
-      </v-card>
-    </v-app>
-  </div>
+                    <v-btn icon>
+                        <v-icon color="white"> mdi-facebook </v-icon>
+                    </v-btn>
 
+                    <v-btn icon>
+                        <v-icon color="white"> mdi-linkedin</v-icon>
+                    </v-btn>
+                </v-app-bar>
 
+                <v-navigation-drawer v-model="drawer" absolute temporary>
+                    <v-list nav dense>
+                        <v-list-item-group
+                            v-model="group"
+                            active-class="deep-purple--text text--accent-4"
+                        >
+                            <v-sheet color="grey lighten-4" class="pa-4">
+                                <v-avatar>
+                                    <img
+                                        src="https://tc.com.br/wp-content/uploads/2021/07/steve-jobs.jpg"
+                                        alt="John"
+                                    />
+                                </v-avatar>
+                                <div>josemaria@gmail.com</div>
+                            </v-sheet>
+                            <v-list-item>
+                                <v-list-item-icon>
+                                    <v-icon>mdi-account</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-title>Conta</v-list-item-title>
+                            </v-list-item>
+
+                            <v-list-item>
+                                <v-list-item-icon>
+                                    <v-icon>mdi-cart</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-title
+                                    >Assinatura</v-list-item-title
+                                >
+                            </v-list-item>
+
+                            <v-list-item>
+                                <v-list-item-icon>
+                                    <v-icon>mdi-logout</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-title>Logout</v-list-item-title>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-card>
+        </v-app>
+    </div>
 </template>
 
-    <v-main>
-      <!--  -->
-    </v-main>
+		<v-main>
+			<!--  -->
+		</v-main>
 
 <script>
-  export default {
-    data: () => ({ drawer: null,
-      links: [
-        ['mdi-account', 'Conta'],
-        ['mdi-cart', 'Assinatura'],
-        ['mdi-logout', 'Logout'],
+export default {
+    data: () => ({
+        drawer: null,
+        links: [
+            ["mdi-account", "Conta"],
+            ["mdi-cart", "Assinatura"],
+            ["mdi-logout", "Logout"],
         ],
     }),
-  }
+};
 </script>
-
-<style scoped>
-
-
-
-</style>
