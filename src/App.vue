4c39ac7502b2ef740bbaa3bed4a53148a3d40d31
Flora Barbosa Santiago <oliveira.flora@hotmail.com>
2022-04-05 12:54:38 -0300

    Corpo 1

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
         <v-app id="inspire">
             <v-navigation-drawer v-model="drawer" absolute temporary app>
                 <v-list nav dense>
-                    <v-list-item-group 
-                        
+                    <v-list-item-group
                         active-class="deep-purple--text text--accent-4"
                     >
                         <v-sheet>
@@ -14,11 +13,10 @@
                                     alt="John"
                                 />
                             </v-avatar>
-                            
-							<div>josemaria@gmail.com</div>
 
+                            <div>josemaria@gmail.com</div>
                         </v-sheet>
-                        
+
                         <v-list-item>
                             <v-list-item-icon>
                                 <v-icon>mdi-account</v-icon>
@@ -39,73 +37,33 @@
                             </v-list-item-icon>
                             <v-list-item-title>Logout</v-list-item-title>
                         </v-list-item>
-
                     </v-list-item-group>
                 </v-list>
             </v-navigation-drawer>
-            
-            <v-app-bar 
-				app 
-				extension
-				flat
-				width="100%"
-				class="cabecalho"
-			>
 
-                <v-app-bar-nav-icon
-                    @click="drawer = true"
-                ></v-app-bar-nav-icon>
+            <v-app-bar app extension flat width="100%" class="cabecalho">
+                <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-				<v-avatar class="logo" tile size="70">
-					<v-img src="src/img/logo.png"></v-img>
-				</v-avatar>
+                <v-avatar class="logo" tile size="70">
+                    <v-img src="src/img/logo.png"></v-img>
+                </v-avatar>
 
                 <v-spacer></v-spacer>
 
-                <v-btn 
-					text 
-					class="cabecalho_menu"
-				>
-					Home
-				</v-btn>
+                <v-btn text class="cabecalho_menu"> Home </v-btn>
 
-                <v-btn
-					text 
-					class="cabecalho_menu"
-				>
-					Produtos
-				</v-btn>
-                
-				<v-btn
-					text 
-					class="cabecalho_menu"
-				>
-					Preços
-				</v-btn>
+                <v-btn text class="cabecalho_menu"> Produtos </v-btn>
 
-                <v-btn
-					text 
-					class="cabecalho_menu"
-				>
-					Sobre
-				</v-btn>
+                <v-btn text class="cabecalho_menu"> Preços </v-btn>
 
-                <v-btn
-					text 
-					class="cabecalho_menu"
-				>
-					Contato
-				</v-btn>
+                <v-btn text class="cabecalho_menu"> Sobre </v-btn>
 
+                <v-btn text class="cabecalho_menu"> Contato </v-btn>
             </v-app-bar>
 
             <v-main>
-                <v-container 
-					class="topo_home"
-                    fluid
-                    >
-                    <div class="caixa_redes"
-					>
+                <v-container class="topo_home" fluid>
+                    <div class="caixa_redes">
                         <v-btn icon>
                             <v-icon color="white"> mdi-twitter </v-icon>
                         </v-btn>
@@ -118,118 +76,155 @@
                             <v-icon color="white"> mdi-linkedin</v-icon>
                         </v-btn>
                     </div>
-					
-					<div class="letras_rectangle9_primeira white--text">Alcance novos horizontes <br> com o seu negócio</div>
 
-					<div class="letras_rectangle9_segunda white--text">Uma nova forma de realizar suas campanhas de Marketing <br> Digital para captação de clientes.</div>
-					
-					<div class=botao_saiba_mais>
-						<v-btn
-							rounded
-							color="#AB2B2D"
-							dark
-							width="200"
-							large
-							>
-							Saiba Mais
-						</v-btn>
-					</div>
+                    <div class="letras_rectangle9_primeira white--text">
+                        Alcance novos horizontes <br />
+                        com o seu negócio
+                    </div>
+
+                    <div class="letras_rectangle9_segunda white--text">
+                        Uma nova forma de realizar suas campanhas de Marketing
+                        <br />
+                        Digital para captação de clientes.
+                    </div>
+
+                    <div class="botao_saiba_mais">
+                        <v-btn rounded color="#AB2B2D" dark width="200" large>
+                            Saiba Mais
+                        </v-btn>
+                    </div>
                 </v-container>
 
                 <div class="py-1"></div>
 
-				<v-container
-					class="recursos"
-                    fluid
-				>
-                    <div class="letra_titulos">
-                        Recursos
-                    </div>
-                    <div class="letra_titulos2">
-                        Descubra as vantagens das ferramentas disponíveis<br>dos sistemas AutoM
+                <v-container class="recursos" fluid>
+                    <div class="letra_titulos_recursos">Recursos</div>
+                    <div class="letra_titulos_recursos2">
+                        Descubra as vantagens das ferramentas disponíveis<br />dos
+                        sistemas AutoM
                     </div>
                 </v-container>
 
                 <div class="py-8"></div>
 
-                <v-container
-                    class="text-center"
-                >
-
+                <v-container class="text-center">
                     <v-row>
                         <v-col
                             v-for="({ src, text, title }, i) in articles"
                             :key="i"
                             cols="12"
                             md="4"
-                            >
-                            <v-img
-                                :src="src"
-                                class="mb-4"
-                                height="40px"
-                                width="40px"
-                                max-width="100%" 
-                            ></v-img>
-
-                            <h3
-                                class="font-weight-black mb-4 text-uppercase"
-                                v-text="title"
-                            ></h3>
-
+                        >
                             <div
-                                class="title font-weight-light mb-5"
-                                v-text="text"
-                            ></div>
+                                class="d-flex flex-column"
+                                style="align-items: center"
+                            >
+                                <v-img
+                                    :src="src"
+                                    class="mb-4"
+                                    height="40px"
+                                    width="40px"
+                                ></v-img>
 
+                                <h3
+                                    class="
+                                        font-weight-black
+                                        mb-4
+                                        text-uppercase
+                                    "
+                                    v-text="title"
+                                ></h3>
+
+                                <div
+                                    class="title font-weight-light mb-5"
+                                    v-text="text"
+                                ></div>
+                            </div>
                         </v-col>
                     </v-row>
-				</v-container>
-                
+                </v-container>
+
                 <div class="py-8"></div>
 
                 <v-container>
                     <div>
-					<v-img src="src/img/screen.png"></v-img>
+                        <v-img src="src/img/screen.png"></v-img>
                     </div>
                 </v-container>
 
                 <div class="py-8"></div>
 
                 <v-container>
+                    <v-row no-gutters>
+                        <v-col sm="6" md="5" lg="6">
+                            <v-card class="pa-2" tile >
+                                <v-container class="alinhamento_texto">
+                                    <div class="letra_titulos_ferramenta">
+                                        A melhor maneira para <br />
+                                        expandir seu negócio
+                                    </div>
+                                    <div class="letra_titulos_ferramenta2">
+                                        A ferramenta AutoM garante a<br />captação
+                                        de novos clientes com<br />preços
+                                        acessíveis
+                                    </div>
+                                    <div class="botao_saiba_mais2">
+                                        <v-btn rounded color="#AB2B2D" dark width="200" large>
+                                            Saiba Mais
+                                        </v-btn>
+                                    </div>
+                                </v-container>
+                            </v-card>
+                        </v-col>
 
-
+                        <v-col sm="6" md="5" offset-md="2" lg="6" offset-lg="0">
+                            <v-card class="pa-2" tile>
+                                <div>
+                                    <v-img
+                                        src="src/img/foto_notebook.png"
+                                    ></v-img>
+                                </div>
+                            </v-card>
+                        </v-col>
+                    </v-row>
                 </v-container>
-					<v-row no-gutters>
-						<v-col
-						sm="6"
-						md="5"
-						lg="6"
-						>
-						<v-card
-							class="pa-2"
-							tile
-						>
-							.col-sm-6 .col-md-5 .col-lg-6
-						</v-card>
-						</v-col>
-						<v-col
-						sm="6"
-						md="5"
-						offset-md="2"
-						lg="6"
-						offset-lg="0"
-						>
-						<v-card
-							class="pa-2"
-							tile
-						>
-						<div>
-							A melhor maneira para <br> expandir seu negócio
-						</div>
 
-						</v-card>
-						</v-col>
-					</v-row>
+                <v-container>
+                    <v-row no-gutters>
+                        <v-col sm="6" md="5" lg="6">
+                            <v-card class="pa-2" tile >
+                                <div>
+                                    <v-img
+                                        src="src/img/imagem_crie_links.png"
+                                    ></v-img>
+                                </div>
+                            </v-card>
+                        </v-col>
+
+                        <v-col sm="6" md="5" offset-md="2" lg="6" offset-lg="0">
+                            <v-card class="pa-2" tile>
+                                <v-container class="alinhamento_texto2">
+                                    <div class="letra_titulos_ferramentapeq">
+                                        Na ponta dos seus dedos
+                                    </div>
+                                    <div class="letra_titulos_ferramenta">
+                                        Crie links para suas <br />
+                                        redes sociais.
+                                    </div>
+                                    <div class="letra_titulos_ferramentapeq">
+                                        Whatsapp, Instagram, Facebook e muito mais<br />
+                                        em um só e-mail de contato.
+                                    </div>
+                                    <div class="botao_saiba_mais2">
+                                        <v-btn rounded color="#AB2B2D" dark width="200" large>
+                                            Saiba Mais
+                                        </v-btn>
+                                    </div>
+                                </v-container>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
 
             </v-main>
         </v-app>
@@ -248,21 +243,21 @@ export default {
         ],
         articles: [
             {
-            src: 'src/img/Vector1.png',
-            title: 'Campanhas automáticas',
-            text: 'Com o sistema de agendamento é possível programar campanhas automáticas para envio de conteúdo para seus clientes.',
+                src: "src/img/Vector1.png",
+                title: "Campanhas automáticas",
+                text: "Com o sistema de agendamento é possível programar campanhas automáticas para envio de conteúdo para seus clientes.",
             },
             {
-            src: 'src/img/Vector1.png',
-            title: 'Controle de dados',
-            text: 'Obtenha estatísticas do seu público e crie campanhas específicas para cada grupo',
+                src: "src/img/Vector2.png",
+                title: "Controle de dados",
+                text: "Obtenha estatísticas do seu público e crie campanhas específicas para cada grupo",
             },
             {
-            src: 'src/img/Vector2.png',
-            title: 'Conteúdo personalizável',
-            text: 'Os serviços AutoM possibilitam criar modelos totalmente personalizáveis',
+                src: "src/img/Vector3.png",
+                title: "Conteúdo personalizável",
+                text: "Os serviços AutoM possibilitam criar modelos totalmente personalizáveis",
             },
-        ]
+        ],
     }),
 };
 </script>
